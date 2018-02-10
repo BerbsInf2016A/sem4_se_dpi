de555e389cb460559a6f60be330867fd99c4233f
Berberich André <berbs1989@googlemail.com>
2018-02-10 20:45:17 +0100

    Small refactoring in the example application

--- src/primenumbers_i39/Application.java
+++ src/primenumbers_i39/Application.java
@@ -14,38 +14,38 @@ public class Application {
     public static void main(String[] args) {
         Context fermatContext = new Context(new FermatOperation());
         List<PrimeFactorization> factorizationsFermat = fermatContext.executeStrategy(10, 1000);
-        List<PrimeFactorization> fermatTwins = twin(factorizationsFermat);
-        List<PrimeFactorization> fermatRep = rep(factorizationsFermat);
-        List<PrimeFactorization> fermatOne = one(factorizationsFermat);
 
-        System.out.println("Fermat-Strategy: Twins: Count: " + count(fermatTwins));
-        System.out.println("Fermat-Strategy: Rep: Count: " + count(fermatRep));
-        System.out.println("Fermat-Strategy: One: Count: " + count(fermatOne));
+        System.out.println("Fermat-Strategy: Twins: Count: " + count(twin(factorizationsFermat)));
+        System.out.println("Fermat-Strategy: Rep: Count: " + count(rep(factorizationsFermat)));
+        System.out.println("Fermat-Strategy: One: Count: " + count(one(factorizationsFermat)));
+
+        System.out.println("count(rep(one(data))): " + count(rep(one(factorizationsFermat))));
 
         System.out.println("Fermat-Strategy: Twins --> grouped ");
-        List<int[]> fermatGroups = group(fermatTwins);
+        List<int[]> fermatGroups = group(twin(factorizationsFermat));
         fermatGroups.forEach(t -> System.out.println("----" + Arrays.toString(t)));
 
         System.out.println("Fermat-Strategy: Twins --> compress ");
-        List<String> fermatCompress = compress(fermatTwins);
+        List<String> fermatCompress = compress(twin(factorizationsFermat));
         fermatCompress.forEach(t -> System.out.println("----" + t));
+
+        // Trial:
 
         Context trialContext = new Context(new TrialDivisionOperation());
         List<PrimeFactorization> factorizationsTrial = trialContext.executeStrategy(10, 1000);
-        List<PrimeFactorization> trialTwins = twin(factorizationsTrial);
-        List<PrimeFactorization> trialRep = rep(factorizationsTrial);
-        List<PrimeFactorization> trialOne = one(factorizationsTrial);
 
-        System.out.println("Trial-Strategy: Twins: Count: " + count(trialTwins));
-        System.out.println("Trial-Strategy: Rep: Count: " + count(trialRep));
-        System.out.println("Trial-Strategy: One: Count: " + count(trialOne));
+        System.out.println("Trial-Strategy: Twins: Count: " + count(twin(factorizationsTrial)));
+        System.out.println("Trial-Strategy: Rep: Count: " + count(rep(factorizationsTrial)));
+        System.out.println("Trial-Strategy: One: Count: " + count(one(factorizationsTrial)));
+
+        System.out.println("count(one(rep(data))): " + count(one(rep(factorizationsTrial))));
 
         System.out.println("Trial-Strategy: Twins --> grouped ");
-        List<int[]> trialGroups = group(trialTwins);
+        List<int[]> trialGroups = group(twin(factorizationsTrial));
         trialGroups.forEach(t -> System.out.println("----" + Arrays.toString(t)));
 
         System.out.println("Fermat-Strategy: Twins --> compress ");
-        List<String> trialCompress = compress(trialTwins);
+        List<String> trialCompress = compress(twin(factorizationsTrial));
         trialCompress.forEach(t -> System.out.println("----" + t));
     }
 }
